3c2d244d13df83dc0ac989a2410434d93724f290
woduf9173@naver.com <woduf9173@naver.com>
2021-10-03 20:06:34 +0900

    pageupload_by_params

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 var fs = require('fs');
 var template = require('./lib/template.js');
+var qs = require('querystring');
+var path = require('path');
+var sanitizeHtml = require('sanitize-html');
 
 app.get('/',function(request, response){
   fs.readdir('./data',function(error,filelist){
@@ -16,7 +19,67 @@ app.get('/',function(request, response){
   });
 });
 
-app.listen(3000,()=>console.log('Example app listening on port 3000!'))
+app.get('/page/:pageId',function(request,response){
+  fs.readdir('./data', function(error, filelist){
+    var filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+      var title = request.params.pageId;
+      var sanitizedTitle = sanitizeHtml(title);
+      var sanitizedDescription = sanitizeHtml(description, {
+        allowedTags:['h1']
+      });
+      var list = template.list(filelist);
+      var html = template.HTML(sanitizedTitle, list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        ` <a href="/create">create</a>
+          <a href="/update?id=${sanitizedTitle}">update</a>
+          <form action="delete_process" method="post">
+            <input type="hidden" name="id" value="${sanitizedTitle}">
+            <input type="submit" value="delete">
+          </form>`
+      );
+      response.send(html);
+    });
+  });
+});
+
+app.get('/create',function(request,response){
+  fs.readdir('./data', function(error, filelist){
+    var title = 'WEB - create';
+    var list = template.list(filelist);
+    var html = template.HTML(title, list, `
+      <form action="/create_process" method="post">
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p>
+          <textarea name="description" placeholder="description"></textarea>
+        </p>
+        <p>
+          <input type="submit">
+        </p>
+      </form>
+    `, '');
+    
+    response.send(html);
+  });
+})
+
+app.post('/create_process',function(request,response){
+  var body="";
+  request.on('data',function(data){
+    body = body+data;
+  });
+  request.on('end',function(){
+    var post = qs.parse(body);
+    var title = post.title;
+    var description = post.description;
+    fs.writeFile(`data/${title}`,description,'utf8',function(err){
+      response.writeHead(302,{Location:`/?id=${title}`});
+      response.end();
+    })
+  })
+});
+
+app.listen(3000,()=>console.log('Example app listening on port 3000!'));
 
 
 /*
